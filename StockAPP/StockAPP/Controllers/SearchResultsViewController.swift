09e382a29d98c345adb43957092b0826225b1eb5
Kenny <kenny55660955@gmail.com>
2021-07-02 23:40:50 +0800

    feat: Delete storyBoard

---
+++ StockAPP/StockAPP/Controllers/SearchResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResultsViewController.swift
+//  StockAPP
+//
+//  Created by Kenny on 2021/7/2.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
